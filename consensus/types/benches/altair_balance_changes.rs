0388c16c63df0854a29952c061d80c684c366767
Paul Hauner <paul@paulhauner.com>
2021-08-06 15:50:05 +1000

    Add benches

--- consensus/types/benches/altair_balance_changes.rs
+++ consensus/types/benches/altair_balance_changes.rs
@@ -3,14 +3,14 @@
 use criterion::Criterion;
 use criterion::{black_box, criterion_group, criterion_main, Benchmark};
 use eth2::{types::StateId, BeaconNodeHttpClient, SensitiveUrl, Timeouts};
-use ssz::Encode;
 use std::sync::Arc;
 use std::time::Duration;
 use tokio::runtime::Runtime;
-use types::{BeaconState, EthSpec, MainnetEthSpec, Validator};
+use types::{BeaconState, CloneConfig, EthSpec, MainnetEthSpec};
 
 const BEACON_NODE_URL: &'static str = "http://localhost:5052";
-const STATE_SLOT: u64 = 1772320;
+const STATE_SLOT: u64 = 1783731;
+const SYNC_COMMITTEE_SIZE: usize = 512;
 
 fn get_state<E: EthSpec>() -> BeaconState<E> {
     let client = BeaconNodeHttpClient::new(
@@ -36,86 +36,39 @@ fn all_benches(c: &mut Criterion) {
 
     let mut state = get_state::<MainnetEthSpec>();
     state.build_all_caches(&spec).expect("should build caches");
-    let state_bytes = state.as_ssz_bytes();
+    let _ = state.update_tree_hash_cache().unwrap();
 
-    let validator_count = state.validators().len();
+    let clean_state = state.clone_with(CloneConfig::all());
 
+    let mut dirty_state = state;
+
+    for chunk in dirty_state.balances_mut().chunks_mut(SYNC_COMMITTEE_SIZE) {
+        chunk[0] += 1
+    }
+
+    do_benches(c, "clean", clean_state);
+    do_benches(c, "dirty", dirty_state);
+}
+
+fn do_benches<E: EthSpec>(c: &mut Criterion, title: &str, state: BeaconState<E>) {
     let inner_state = state.clone();
     c.bench(
-        &format!("{}_validators", validator_count),
-        Benchmark::new("encode/beacon_state", move |b| {
+        title,
+        Benchmark::new("cached_tree_hash/initialized_cache", move |b| {
             b.iter_batched_ref(
                 || inner_state.clone(),
-                |state| black_box(state.as_ssz_bytes()),
+                |state| black_box(state.update_tree_hash_cache()),
                 criterion::BatchSize::SmallInput,
             )
         })
-        .sample_size(10),
-    );
-
-    c.bench(
-        &format!("{}_validators", validator_count),
-        Benchmark::new("decode/beacon_state", move |b| {
-            b.iter_batched_ref(
-                || (state_bytes.clone(), spec.clone()),
-                |(bytes, spec)| {
-                    let state: BeaconState<MainnetEthSpec> =
-                        BeaconState::from_ssz_bytes(&bytes, &spec).expect("should decode");
-                    black_box(state)
-                },
-                criterion::BatchSize::SmallInput,
-            )
-        })
-        .sample_size(10),
-    );
-
-    let inner_state = state.clone();
-    c.bench(
-        &format!("{}_validators", validator_count),
-        Benchmark::new("clone/beacon_state", move |b| {
-            b.iter_batched_ref(
-                || inner_state.clone(),
-                |state| black_box(state.clone()),
-                criterion::BatchSize::SmallInput,
-            )
-        })
-        .sample_size(10),
-    );
-
-    let inner_state = state.clone();
-    c.bench(
-        &format!("{}_validators", validator_count),
-        Benchmark::new("clone/tree_hash_cache", move |b| {
-            b.iter_batched_ref(
-                || inner_state.clone(),
-                |state| black_box(state.tree_hash_cache().clone()),
-                criterion::BatchSize::SmallInput,
-            )
-        })
-        .sample_size(10),
-    );
-
-    let inner_state = state.clone();
-    c.bench(
-        &format!("{}_validators", validator_count),
-        Benchmark::new(
-            "initialized_cached_tree_hash_without_changes/beacon_state",
-            move |b| {
-                b.iter_batched_ref(
-                    || inner_state.clone(),
-                    |state| black_box(state.update_tree_hash_cache()),
-                    criterion::BatchSize::SmallInput,
-                )
-            },
-        )
         .sample_size(10),
     );
 
     let mut inner_state = state.clone();
     inner_state.drop_all_caches().unwrap();
     c.bench(
-        &format!("{}_validators", validator_count),
-        Benchmark::new("non_initialized_cached_tree_hash/beacon_state", move |b| {
+        title,
+        Benchmark::new("cached_tree_hash/non_initialized_cache", move |b| {
             b.iter_batched_ref(
                 || inner_state.clone(),
                 |state| {
@@ -128,35 +81,6 @@ fn all_benches(c: &mut Criterion) {
                 criterion::BatchSize::SmallInput,
             )
         })
-        .sample_size(10),
-    );
-
-    let inner_state = state.clone();
-    c.bench(
-        &format!("{}_validators", validator_count),
-        Benchmark::new(
-            "initialized_cached_tree_hash_with_new_validators/beacon_state",
-            move |b| {
-                b.iter_batched_ref(
-                    || {
-                        let mut state = inner_state.clone();
-                        for _ in 0..16 {
-                            state
-                                .validators_mut()
-                                .push(Validator::default())
-                                .expect("should push validatorj");
-                            state
-                                .balances_mut()
-                                .push(32_000_000_000)
-                                .expect("should push balance");
-                        }
-                        state
-                    },
-                    |state| black_box(state.update_tree_hash_cache()),
-                    criterion::BatchSize::SmallInput,
-                )
-            },
-        )
         .sample_size(10),
     );
 }
